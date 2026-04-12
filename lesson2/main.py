348ba5bd118c879e98b9fdcb428e69106f8844d5
Atiya <mimikyu14xy@gmail.com>
2026-04-12 14:14:32 -0400

    sum of number

---
+++ lesson2/main.py
@@ -0,0 +1,13 @@
+n = 5
+#1.range(stop)
+for i in range(n):
+    print(i) 
+
+#2. range(start, stop)
+print("range(start, stop)")
+for i in range(2,n):
+    print(i) 
+#3. range(start, stop, step)
+print("range(start, stop, step)")
+for i in range(1,10,2):
+    print(i) 
